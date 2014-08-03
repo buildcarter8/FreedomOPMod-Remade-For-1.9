c26639e3f2d27cfc4a34fe5bea3aab0af54eea7a
John J. Miller <a784b2a1@opayq.com>
2014-08-03 19:46:49 -0400

    Update Command_glist.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_glist.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_glist.java
@@ -83,7 +83,7 @@ public class Command_glist extends TFM_Command
                 if (target != null)
                 {
                     TFM_BanManager.addUuidBan(new TFM_Ban(TFM_Util.getUuid(target), target.getName()));
-                    target.kickPlayer("You have been banned by " + sender.getName() + "\n If you think you have been banned wrongly, appeal here: http://to.fop.us.to/appeal");
+                    target.kickPlayer("You have been banned by " + sender.getName() + "\n If you think you have been banned wrongly, appeal here: http://www.totalfreedom.boards.net");
                 }
                 else
                 {
@@ -99,7 +99,7 @@ public class Command_glist extends TFM_Command
             }
             else if (mode.equalsIgnoreCase("unban") || mode.equalsIgnoreCase("pardon"))
             {
-                TFM_Util.adminAction(sender.getName(), "Unbanning " + username + " and IPs: " + StringUtils.join(ips, ","), true);
+                TFM_Util.adminAction(sender.getName(), "Unbanning " + username, true);
                 TFM_BanManager.unbanUuid(TFM_Util.getUuid(username));
                 for (String ip : ips)
                 {
